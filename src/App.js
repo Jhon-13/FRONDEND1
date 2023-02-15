f4a79d25522b981d29254ee583b740018d76a2a7
Jhon-13 <jhonzambranob13@gmail.com>
2023-02-14 23:08:59 -0500

    Mi primer github

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import data from './data';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Integracion de Software</a>
+      </header>
+
+      <main>
+        <h1>Productos</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+
+              <div className="product.info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>soy jhon zz</p>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+
+                <button>Añadir a carrito</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Caminsa Nike',
+      slug: 'camisa-nike',
+      category: 'Camisetas',
+      image: '/image/p1.jpg',
+      price: 120,
+      countInStock: 0,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Camisa de alta calidad',
+    },
+
+    {
+      name: 'Caminsa Adidas',
+      slug: 'camisa-adidas',
+      category: 'Camisetas',
+      image: '/image/p2.jpg',
+      price: 150,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'Camisa de alta calidad',
+    },
+
+    {
+      name: 'Pantalon Nike',
+      slug: 'pantalon-nike',
+      category: 'Pantalones',
+      image: '/image/p3.jpg',
+      price: 250,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'Pantalon de alta calidad',
+    },
+
+    {
+      name: 'Pantalon Adidas',
+      slug: 'pantalon-adidas',
+      category: 'Pantalones',
+      image: '/image/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Pantalones de alta calidad',
+    },
+  ],
+};
+
+export default data;
