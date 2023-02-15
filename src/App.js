a5bfaed015eb4c8d88c439c71be41bf10160081b
Jhon-13 <jhonzambranob13@gmail.com>
2023-02-14 23:27:58 -0500

    config

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
                 <a href={`/product/${product.slug}`}>
                   <p>{product.name}</p>
                 </a>
-                <p>soy jhon zz</p>
+                <p>soy jhon zzm</p>
                 <p>
                   <strong>${product.price}</strong>
                 </p>
